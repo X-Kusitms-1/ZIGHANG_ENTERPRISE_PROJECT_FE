a3d0db6907adc8ade15fc73195b83d767f40c3f2
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-08-29 00:14:15 +0900

    ✨ Feat: 패키지 충돌 해결 및 스토리북 컴포넌트 폴더 통일
    feat: 패키지 충돌 해결 및 스토리북 컴포넌트 폴더 통일

--- src/stories/FilterButton.stories.tsx
+++ src/stories/FilterButton.stories.tsx
@@ -6,7 +6,7 @@ import type { Meta } from "@storybook/nextjs";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/FilterButton",
+  title: "Components/FilterButton",
   component: FilterButton,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
