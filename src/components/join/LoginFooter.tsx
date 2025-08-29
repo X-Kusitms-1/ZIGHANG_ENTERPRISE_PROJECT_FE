1eb99f53cd0ae3cc32f417c6508d758da6830c98
dasosann <goodtony0403@gmail.com>
2025-08-30 01:11:43 +0900

    feat: 로그인 푸터 컴포넌트 추가

---
+++ src/components/join/LoginFooter.tsx
@@ -0,0 +1,9 @@
+export default function LoginFooter(){
+    return(
+        <div className="flex w-full flex-row items-center justify-center text-[12px] text-[#363636]">
+            <a className="hover:underline" href="https://quasar-guava-e9f.notion.site/1f5144e6653280379023dacb5593b2dd?pvs=74">개인정보 처리 방침</a>
+            <div className="mx-2">|</div>
+            <a className="hover:underline" href="https://quasar-guava-e9f.notion.site/1f5144e6653280fe9217f4a46a343de0">이용 약관</a>
+        </div>
+    );
+}
