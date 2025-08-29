41fc14e64d7bf9616b2b5a1ac3dc0f6ec36fb536
dasosann <goodtony0403@gmail.com>
2025-08-30 01:15:09 +0900

    feat: 로그인 페이지 완성

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,4 +1,7 @@
-import LoginTitle from "./LoginTitle";
+import LoginFooter from "../../components/join/LoginFooter";
+import LoginSocialList from "../../components/join/LoginSocialList";
+import LoginSubTitle from "../../components/join/LoginSubTitle";
+import LoginTitle from "../../components/join/LoginTitle";
 
 export default function LoginPage() {
   return (
@@ -7,8 +10,10 @@ export default function LoginPage() {
         <section className="tablet:gap-[50px] flex h-screen w-full flex-col items-center justify-center gap-10">
           <div className="flex flex-col items-center gap-5">
             <LoginTitle/>
-            
+            <LoginSubTitle/>
           </div>
+          <LoginSocialList/>
+          <LoginFooter/> 
         </section>
       </div>
     </main>
