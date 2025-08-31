509684914376d691a2ebf2162d678282feebfbb5
dasosann <goodtony0403@gmail.com>
2025-08-31 23:29:36 +0900

    feat: HeaderLogin 컴포넌트 추가

---
+++ src/components/join/HeaderLogin.tsx
@@ -0,0 +1,8 @@
+export default function HeaderLogin(){
+    return (
+        <div className="flex items-center gap-5 w-full h-[96px] px-30 py-4">
+            <img src="/join/header-logo.svg" alt="header-logo" className="w-20 h-10"/>
+            <div className="text-20-600 text-neutral-700">직행 통합 로그인</div>
+        </div>
+    );
+}
