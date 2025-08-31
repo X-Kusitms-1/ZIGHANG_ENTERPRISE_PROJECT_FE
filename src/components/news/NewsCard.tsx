e7d1cacd9a3776a8b5a50c2d375e9e0f37f7346f
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-08-31 20:50:40 +0900

    💄 Design: 기업 상세 페이지 디자인
    design: 기업 상세 페이지 디자인

--- src/components/news/NewsCard.tsx
+++ src/components/news/NewsCard.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import Image from "next/image";
+import Link from "next/link";
 
 interface NewsCard {
   id: string; // 뉴스ID
@@ -14,8 +15,9 @@ interface NewsCard {
 const newsCardVariants = cva("news-card", {
   variants: {
     variant: {
-      main: "flex flex-col space-y-4 w-full max-w-[300px] max-tablet:max-w-[180px]",
+      main: "flex flex-col space-y-4 w-full max-w-[300px] max-tablet:max-w-[180px] group",
       sub: "flex flex-col max-tablet:items-center space-x-2 space-y-4 w-full max-w-[300px] max-tablet:max-w-[365px]  max-tablet:flex-row",
+      text: "border-b-border-line border-b py-3",
     },
   },
   defaultVariants: {
@@ -29,25 +31,32 @@ type NewsCardProps = VariantProps<typeof newsCardVariants> & {
 
 function NewsCard({ newsCard, variant }: NewsCardProps) {
   return (
-    <div className={newsCardVariants({ variant })}>
+    <Link
+      href={`/news/${newsCard.id}`}
+      className={newsCardVariants({ variant })}
+    >
       <Image
         src={newsCard.image}
         alt={newsCard.title}
         width={300}
         height={200}
-        className={`h-full w-full rounded-[8px] object-cover ${
+        className={`min-h-[130px] w-full rounded-[8px] object-cover ${
           variant === "sub" ? "max-tablet:h-[64px] max-tablet:w-[100px]" : ""
-        }`}
+        } ${variant === "text" ? "hidden" : ""}`}
       />
       <div className="flex flex-col gap-2">
-        <h2 className="text-16-600 text-text-primary max-tablet:text-14-600">
+        <h2
+          className={`text-16-600 text-text-primary max-tablet:text-14-600 group-hover:underline ${
+            variant === "text" ? "hover:underline" : ""
+          }`}
+        >
           {newsCard.title}
         </h2>
         <div className="text-12-500 text-text-tertiary flex items-center">
           <p>{newsCard.createdAt}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
