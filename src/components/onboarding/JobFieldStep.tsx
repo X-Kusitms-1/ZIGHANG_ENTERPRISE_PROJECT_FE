49cc69dd4c602f030ff235f3273cd43313f3cef5
dasosann <goodtony0403@gmail.com>
2025-09-03 02:38:08 +0900

    feat: JobFieldStep 컴포넌트에 Modal에서 props 내려받음 및 JobItem 타입 정의

--- src/components/onboarding/JobFieldStep.tsx
+++ src/components/onboarding/JobFieldStep.tsx
@@ -1,9 +1,31 @@
 import JobCategoryGrid from "./JobCategoryGrid";
 
-export default function JobFieldStep() {
+type JobItem = {
+  jobFamily: string;
+  role: string;
+};
+
+interface JobFieldStepProps {
+  jobList: JobItem[];
+  setJobList: React.Dispatch<React.SetStateAction<JobItem[]>>;
+  isUndecided: boolean;
+  setIsUndecided: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function JobFieldStep({
+  jobList,
+  setJobList,
+  isUndecided,
+  setIsUndecided,
+}: JobFieldStepProps) {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-6">
-      <JobCategoryGrid />
+      <JobCategoryGrid
+        jobList={jobList}
+        setJobList={setJobList}
+        isUndecided={isUndecided}
+        setIsUndecided={setIsUndecided}
+      />
     </div>
   );
 }
