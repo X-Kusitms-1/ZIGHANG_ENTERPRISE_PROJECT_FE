c5398caccb507002f205373c5eee3fa56c632201
dasosann <goodtony0403@gmail.com>
2025-09-04 23:44:20 +0900

    fix: 이미지 경로 수정 및 텍스트 스타일 일관성 유지

--- src/components/join/LoginTitle.tsx
+++ src/components/join/LoginTitle.tsx
@@ -2,7 +2,7 @@
 
 export default function LoginTitle() {
   return (
-    <div className="text-[28px] text-center font-semibold mb-3">
+    <div className="text-28-600 text-center mb-3">
         내 직군의 채용 공고를 매일 받아 보세요!
     </div>
   );
