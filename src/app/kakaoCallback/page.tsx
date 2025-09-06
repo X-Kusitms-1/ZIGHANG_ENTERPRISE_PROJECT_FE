919bd8a74c6c8c83589c4d76191ca64b8a98002b
dasosann <goodtony0403@gmail.com>
2025-09-07 00:46:34 +0900

    feat: KakaoCallback리다이렉트 페이지 추가

---
+++ src/app/kakaoCallback/page.tsx
@@ -0,0 +1,7 @@
+export default function KakaoCallbackPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-t-4 border-b-4 border-purple-500" />
+    </div>
+  );
+}
