d22ad16d40e1f9f2214ece4cddb63332199ecd5c
dasosann <goodtony0403@gmail.com>
2025-09-08 00:53:17 +0900

    feat: MobileLoginTitle 및 MobileSubTitle 컴포넌트 확장성으로className prop 추가

--- src/components/mobileLogin/MobileSubTitle.tsx
+++ src/components/mobileLogin/MobileSubTitle.tsx
@@ -3,17 +3,18 @@ import { ReactNode } from "react";
 
 interface MobileSubTitleProps {
   text?: ReactNode;
+  className?: string;
 }
 
-export default function MobileSubTitle({ text }: MobileSubTitleProps) {
+export default function MobileSubTitle({ text, className }: MobileSubTitleProps) {
   return (
-    <div className="text-16-500 text-text-tertiary leading-6">
+    <div className={`text-16-500 text-text-tertiary leading-6${className ? ` ${className}` : ""}`}>
       {text ?? (
-        <>
+        <p>
           당일 올라온 채용 공고 10개를 매일 아침
           <br className="tablet:hidden" />
           이메일로 보내드려요!
-        </>
+        </p>
       )}
     </div>
   );
