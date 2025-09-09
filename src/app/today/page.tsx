18bfbbb616f6070a011d0b11cb75e0292ff92c1e
dasosann <goodtony0403@gmail.com>
2025-09-10 00:22:10 +0900

    feat: 오늘의 지원 페이지 레이아웃 개선 및 페이지 추가

--- src/app/today/page.tsx
+++ src/app/today/page.tsx
@@ -1,15 +1,19 @@
 import ApplyCountInput from "@/components/today/ApplyCountInput";
+import MainSqureGroup from "@/components/today/MainSqureGroup";
+import SideGroup from "@/components/today/SideComponent";
 import { TodaysGoalStatus } from "@/components/today/TodaysGoalStatus";
 
 export default function TodaysApply(){
     return(
-        <div className="bg-bg-neutral h-100dvh w-full pt-12 px-30 mx-auto">
+        <div className="bg-bg-neutral h-100dvh w-full pt-12 px-30 mx-auto flex gap-3">
             <div className="flex gap-3">
                 <ApplyCountInput/>
-                <div className="w-[402px]">
+                <div className="w-[402px] flex flex-col gap-3">
                     <TodaysGoalStatus />
+                    <MainSqureGroup/>
                 </div>
             </div>
+            <SideGroup/>
         </div>
     );
 }
