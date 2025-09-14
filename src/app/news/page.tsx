afe8e2c035aba47080cce527fc8e2fc0d861ca15
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-09-14 22:07:59 +0900

    fix: 페이지에서 모달 삭제

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -8,7 +8,6 @@ import CompanyRow from "@/components/news/CompanyRow";
 import getQueryClient from "@/utils/getQueryClient";
 import TotalNewsSkeleton from "@/components/news/TotalNewsSkeleton";
 import { NewsFilterProvider } from "@/context/NewsFilterContext";
-import AnnouncementModal from "@/components/today/AnnouncementModal";
 
 function NewsPage() {
   const queryClient = getQueryClient();
@@ -33,7 +32,6 @@ function NewsPage() {
             <FilterModal />
           </div>
           <SubscribedFilterButton className="gap-1" />
-          <AnnouncementModal />
         </section>
         <section className="max-pc:hidden mt-12 w-full max-w-[1200px]">
           <Suspense fallback={<TotalNewsSkeleton />}>
