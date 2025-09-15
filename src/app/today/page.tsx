43c308522d47a8640f71f0b5e17928c8df91dbc2
dasosann <goodtony0403@gmail.com>
2025-09-15 17:15:16 +0900

    feat: 오늘의지원 페이지 w 길이 수정 및 svg 및 png 파일 추가

--- src/app/today/page.tsx
+++ src/app/today/page.tsx
@@ -4,20 +4,22 @@ import MainSqureGroup from "@/components/today/MainSqureGroup";
 import SideGroup from "@/components/today/SideComponent";
 import { TodaysGoalStatus } from "@/components/today/TodaysGoalStatus";
 
-export default function TodaysApply(){
-    return(
-        <div className="bg-bg-neutral h-100dvh w-full pt-12 mx-auto flex gap-3 justify-center">
-            <div className="flex flex-col gap-11">
-                <div className="flex gap-3">
-                    <ApplyCountInput/>
-                    <div className="w-[402px] flex flex-col gap-3">
-                        <TodaysGoalStatus />
-                        <MainSqureGroup/>
-                    </div>
-                </div>
-                <div className="mr-[10px]"><ApplyList/></div>
-            </div>
-            <SideGroup/>
+export default function TodaysApply() {
+  return (
+    <div className="bg-bg-neutral tablet:px-6 flex min-h-screen w-screen justify-center gap-3 px-4 pt-12">
+      <div className="flex flex-col gap-11">
+        <div className="flex gap-3">
+          <ApplyCountInput />
+          <div className="flex w-[402px] flex-col gap-3">
+            <TodaysGoalStatus />
+            <MainSqureGroup />
+          </div>
         </div>
-    );
+        <div className="mr-[10px]">
+          <ApplyList />
+        </div>
+      </div>
+      <SideGroup />
+    </div>
+  );
 }
